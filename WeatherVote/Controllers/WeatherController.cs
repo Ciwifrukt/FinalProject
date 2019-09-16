47377090ec6451b922483c89eb54d606533ca89b
pezongod <pezongod@gmail.com>
2019-09-16 13:36:15 +0200

    ---

---
+++ WeatherVote/Controllers/WeatherController.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WeatherVote.Controllers
+{
+    public class WeatherController : Controller
+    {
+        // GET: Weather
+        public ActionResult Index()
+        {
+            var xxx = new Rootobjectsmhi();
+            return View();
+        }
+
+        // GET: Weather/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Weather/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Weather/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Weather/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Weather/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Weather/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Weather/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
--- WeatherVote/Models/SMHI.cs
+++ WeatherVote/Models/SMHI.cs
@@ -9,7 +9,7 @@ namespace WeatherVote.Models
     public class SMHI
     {
 
-        public class Rootobject
+        public class Rootobjectsmhi
         {
             public DateTime approvedTime { get; set; }
             public DateTime referenceTime { get; set; }
@@ -38,23 +38,6 @@ namespace WeatherVote.Models
             public float[] values { get; set; }
         }
 
-        //public Rootobject xxx { get; set; }
-        //public string TempDay { get; set; }
-        //readonly HttpClient client = new HttpClient();
-        //public async Task OnGetAsync()
-        //{
-        //    var s = "";
-        //    var url = "https://opendata-download-metfcst.smhi.se/api/category/pmp3g/version/2/geotype/point/lon/17.9446/lat/59.40719/data.json";
-
-        //    //var respons = await client.GetAsync(url);
-        //    //s = await respons.Content.ReadAsStringAsync();
-        //    // Above three lines can be replaced with new helper method below
-        //    s = await client.GetStringAsync(url);
-
-        //    var d = new DateTime(2019, 9, 11, 15, 0, 0);
-        //    xxx = JsonConvert.DeserializeObject<Rootobject>(s);
-        //    var temperature = xxx.timeSeries.Single(x => x.validTime == d).parameters.Single(x => x.);
-
-        //}
+        
     }
 }
