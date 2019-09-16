c11d7029ea56c8bfb74a1299ca3c03603013720e
Ciwifrukt <v.strandberg@outlook.com>
2019-09-16 10:42:51 +0200

    klasser skapade

---
+++ WeatherVote/Models/Meto.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeatherVote.Models
+{
+    public class Meto
+    {
+
+        public class Rootobject
+        {
+            public Locations Locations { get; set; }
+        }
+
+        public class Locations
+        {
+            public Location[] Location { get; set; }
+        }
+
+        public class Location
+        {
+            public string elevation { get; set; }
+            public string id { get; set; }
+            public string latitude { get; set; }
+            public string longitude { get; set; }
+            public string name { get; set; }
+            public string region { get; set; }
+            public string unitaryAuthArea { get; set; }
+            public string obsSource { get; set; }
+            public string nationalPark { get; set; }
+        }
+
+    }
+}
---
+++ WeatherVote/Models/SMHI.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SMHIapp.Models
+{
+    public class SMHI
+    {
+
+        public class Rootobject
+        {
+            public DateTime approvedTime { get; set; }
+            public DateTime referenceTime { get; set; }
+            public Geometry geometry { get; set; }
+            public Timesery[] timeSeries { get; set; }
+        }
+
+        public class Geometry
+        {
+            public string type { get; set; }
+            public float[][] coordinates { get; set; }
+        }
+
+        public class Timesery
+        {
+            public DateTime validTime { get; set; }
+            public Parameter[] parameters { get; set; }
+        }
+
+        public class Parameter
+        {
+            public string name { get; set; }
+            public string levelType { get; set; }
+            public int level { get; set; }
+            public string unit { get; set; }
+            public float[] values { get; set; }
+        }
+
+    }
+}
