89d395407397942e7229cc926fb124c831c853b8
Ciwifrukt <v.strandberg@outlook.com>
2019-09-18 09:59:51 +0200

    layout updated

--- WeatherVote/Controllers/HomeController.cs
+++ WeatherVote/Controllers/HomeController.cs
@@ -28,5 +28,10 @@ namespace WeatherVote.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult WeatherAgency()
+        {
+            return View();
+        }
     }
 }
