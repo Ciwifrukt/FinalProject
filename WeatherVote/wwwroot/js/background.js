cb0c33008d258057f6ac7b00580466235a4ab693
Muss Mesmari <m.messmari@users.noreply.github.com>
2019-09-24 14:31:14 +0200

    more bugs fixed

--- WeatherVote/wwwroot/js/background.js
+++ WeatherVote/wwwroot/js/background.js
@@ -5,7 +5,8 @@ if (t < 10) {
     //document.body.style.backgroundImage = "url('../img/sky.jpg')";
 } else if (t < 20) {
     //document.body.style.backgroundImage = "url('../img/sunset.jpg')";
-    document.body.style.backgroundImage = "url('../img/stars.jpg')";
+    //document.body.style.backgroundImage = "url('../img/stars.jpg')";
+    document.body.style.backgroundImage = "url('../img/sky.jpg')";
 } else {
     //document.body.style.backgroundImage = "url('../img/stars.jpg')";
     document.body.style.backgroundImage = "url('../img/sky.jpg')";
