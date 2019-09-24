346e85b633c5d129119658165f237c7f32bb89dd
Ciwifrukt <v.strandberg@outlook.com>
2019-09-24 16:41:04 +0200

    dropdown med javascript

--- WeatherVote/wwwroot/js/site.js
+++ WeatherVote/wwwroot/js/site.js
@@ -1,4 +1,14 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿
+let qqq
+function showa(t) {
 
-// Write your JavaScript code.
+    qqq = $(".weather-body", t)
+
+if (qqq.css('display') == 'none') {
+    qqq.css("display", "block");
+}
+else {
+    qqq.css("display", "none");
+
+    }
+}
