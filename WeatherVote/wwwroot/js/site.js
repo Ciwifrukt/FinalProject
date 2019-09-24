a0d9236beb6d4aa8d9155c124aa8a63954f9ec23
Ciwifrukt <v.strandberg@outlook.com>
2019-09-24 16:54:20 +0200

    bugfix att den fäldes ut när man likeda

--- WeatherVote/wwwroot/js/site.js
+++ WeatherVote/wwwroot/js/site.js
@@ -4,11 +4,15 @@ function showa(t) {
 
     qqq = $(".weather-body", t)
 
-if (qqq.css('display') == 'none') {
-    qqq.css("display", "block");
-}
-else {
-    qqq.css("display", "none");
+    if (!$(event.target).closest(".btn", t).length) {
 
+        if (qqq.css('display') == 'none') {
+            qqq.css("display", "block");
+        }
+
+        else {
+            qqq.css("display", "none");
+
+        }
     }
 }
