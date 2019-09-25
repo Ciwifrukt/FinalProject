833bbc9adde818025594512604b7bd284254edd5
Muss Mesmari <m.messmari@users.noreply.github.com>
2019-09-25 10:44:26 +0200

    FIXED BUGS

--- WeatherVote/wwwroot/js/background.js
+++ WeatherVote/wwwroot/js/background.js
@@ -1,13 +1,13 @@
 ﻿var t = new Date().getHours();
 
-if (t < 10) {
+if (t < 12) {
+    //document.body.style.backgroundImage = "url('../img/stars.jpg')";
+    document.body.style.backgroundImage = "url('../img/sky.jpg')";
+} else if (t < 16) {
+    document.body.style.backgroundImage = "url('../img/sunset.jpg')";
+    //document.body.style.backgroundImage = "url('../img/stars.jpg')";
+    //document.body.style.backgroundImage = "url('../img/sky.jpg')";
+} else {
     document.body.style.backgroundImage = "url('../img/stars.jpg')";
     //document.body.style.backgroundImage = "url('../img/sky.jpg')";
-} else if (t < 20) {
-    //document.body.style.backgroundImage = "url('../img/sunset.jpg')";
-    //document.body.style.backgroundImage = "url('../img/stars.jpg')";
-    document.body.style.backgroundImage = "url('../img/sky.jpg')";
-} else {
-    //document.body.style.backgroundImage = "url('../img/stars.jpg')";
-    document.body.style.backgroundImage = "url('../img/sky.jpg')";
-}
+       }
--- WeatherVote/wwwroot/js/site.js
+++ WeatherVote/wwwroot/js/site.js
@@ -4,11 +4,11 @@ function showa(t) {
 
     qqq = $(".weather-body", t)
 
-if (qqq.css('display') == 'none') {
-    qqq.css("display", "block");
-}
-else {
-    qqq.css("display", "none");
+    if (qqq.css('display') == 'none') {
+        qqq.css("display", "block");
+    }
+    else {
+        qqq.css("display", "none");
 
     }
 }
