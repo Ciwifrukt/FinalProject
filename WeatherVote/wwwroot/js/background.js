ce2413282caf35a33f5756088c1a5f64504968d9
Ciwifrukt <v.strandberg@outlook.com>
2019-09-25 10:51:41 +0200

    fixat merge

--- WeatherVote/wwwroot/js/background.js
+++ WeatherVote/wwwroot/js/background.js
@@ -7,8 +7,7 @@ if (t < 12) {
     document.body.style.backgroundImage = "url('../img/sunset.jpg')";
     //document.body.style.backgroundImage = "url('../img/stars.jpg')";
     //document.body.style.backgroundImage = "url('../img/sky.jpg')";
-    //document.body.style.backgroundImage = "url('../img/stars.jpg')";
-    //document.body.style.backgroundImage = "url('../img/sky.jpg')";
+
 } else {
     document.body.style.backgroundImage = "url('../img/stars.jpg')";
     //document.body.style.backgroundImage = "url('../img/sky.jpg')";
--- WeatherVote/wwwroot/js/site.js
+++ WeatherVote/wwwroot/js/site.js
@@ -4,13 +4,6 @@ function showa(t) {
 
     qqq = $(".weather-body", t)
 
-<<<<<<< HEAD
-    if (qqq.css('display') == 'none') {
-        qqq.css("display", "block");
-    }
-    else {
-        qqq.css("display", "none");
-=======
     if (!$(event.target).closest(".btn", t).length) {
 
         if (qqq.css('display') == 'none') {
@@ -19,7 +12,7 @@ function showa(t) {
 
         else {
             qqq.css("display", "none");
->>>>>>> 87cf20b8dfa2f5b7b9935965f681992c8e45345e
+
 
         }
     }
