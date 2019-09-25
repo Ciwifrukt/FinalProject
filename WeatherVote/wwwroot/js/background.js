87cf20b8dfa2f5b7b9935965f681992c8e45345e
Ciwifrukt <v.strandberg@outlook.com>
2019-09-25 09:20:26 +0200

    bakgrund fix

--- WeatherVote/wwwroot/js/background.js
+++ WeatherVote/wwwroot/js/background.js
@@ -1,12 +1,12 @@
 ﻿var t = new Date().getHours();
 
 if (t < 10) {
-    document.body.style.backgroundImage = "url('../img/stars.jpg')";
-    //document.body.style.backgroundImage = "url('../img/sky.jpg')";
-} else if (t < 20) {
-    //document.body.style.backgroundImage = "url('../img/sunset.jpg')";
     //document.body.style.backgroundImage = "url('../img/stars.jpg')";
     document.body.style.backgroundImage = "url('../img/sky.jpg')";
+} else if (t < 20) {
+    document.body.style.backgroundImage = "url('../img/sunset.jpg')";
+    //document.body.style.backgroundImage = "url('../img/stars.jpg')";
+    //document.body.style.backgroundImage = "url('../img/sky.jpg')";
 } else {
     //document.body.style.backgroundImage = "url('../img/stars.jpg')";
     document.body.style.backgroundImage = "url('../img/sky.jpg')";
